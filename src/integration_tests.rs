da913982907a9b42d40b78389455c539c81d8ed4
Anton Jansson <anton_jansson@live.se>
2022-10-13 19:44:27 +0200

    Add integration tests for count metric.

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -3,7 +3,7 @@ use lazy_static::lazy_static;
 use serde::Deserialize;
 use tempfile::tempdir;
 
-use crate::{DefaultGaugeMetric, PrimaryTag, Query, TagsFilter, TimeRange, TransformOperation};
+use crate::{DefaultCountMetric, DefaultGaugeMetric, PrimaryTag, Query, TagsFilter, TimeRange, TransformOperation};
 
 #[derive(Deserialize)]
 struct SampleData {
@@ -236,5 +236,79 @@ fn test_gauge_primary_tag_95th1() {
         0.8006655905325116,
         metric.percentile(Query::new(TimeRange::new(start_time, end_time)), 95).unwrap_or(0.0),
         epsilon = 1e-5
+    );
+}
+
+#[test]
+fn test_count_sum1() {
+    let temp_metric_data = tempdir().unwrap();
+
+    let start_time = 1654077600.0 + 6.0 * 24.0 * 3600.0;
+    let end_time = start_time + 2.0 * 3600.0;
+
+    let mut metric = DefaultCountMetric::new(temp_metric_data.path()).unwrap();
+
+    for index in 0..SAMPLE_DATA.times.len() {
+        metric.add(SAMPLE_DATA.times[index], 1, &[]).unwrap();
+
+        if SAMPLE_DATA.times[index] >= end_time + 3600.0 {
+            break;
+        }
+    }
+
+    assert_eq!(Some(144328.0), metric.sum(Query::new(TimeRange::new(start_time, end_time))));
+}
+
+#[test]
+fn test_count_primary_tag_sum1() {
+    let temp_metric_data = tempdir().unwrap();
+
+    let start_time = 1654077600.0 + 6.0 * 24.0 * 3600.0;
+    let end_time = start_time + 2.0 * 3600.0;
+    let tags_list = vec!["tag:T1", "tag:T2"];
+
+    let mut metric = DefaultCountMetric::new(temp_metric_data.path()).unwrap();
+    metric.add_primary_tag(PrimaryTag::Named("tag:T1".to_owned())).unwrap();
+    metric.add_primary_tag(PrimaryTag::Named("tag:T2".to_owned())).unwrap();
+
+    for index in 0..SAMPLE_DATA.times.len() {
+        let tags = &[tags_list[(index % 2)]];
+        metric.add(SAMPLE_DATA.times[index], 1, tags).unwrap();
+
+        if SAMPLE_DATA.times[index] >= end_time + 3600.0 {
+            break;
+        }
+    }
+
+    assert_eq!(Some(144328.0), metric.sum(Query::new(TimeRange::new(start_time, end_time))));
+}
+
+#[test]
+fn test_count_primary_tag_sum2() {
+    let temp_metric_data = tempdir().unwrap();
+
+    let start_time = 1654077600.0 + 6.0 * 24.0 * 3600.0;
+    let end_time = start_time + 2.0 * 3600.0;
+    let tags_list = vec!["tag:T1", "tag:T2"];
+
+    let mut metric = DefaultCountMetric::new(temp_metric_data.path()).unwrap();
+    metric.add_primary_tag(PrimaryTag::Named("tag:T1".to_owned())).unwrap();
+    metric.add_primary_tag(PrimaryTag::Named("tag:T2".to_owned())).unwrap();
+
+    for index in 0..SAMPLE_DATA.times.len() {
+        let tags = &[tags_list[(index % 2)]];
+        metric.add(SAMPLE_DATA.times[index], 1, tags).unwrap();
+
+        if SAMPLE_DATA.times[index] >= end_time + 3600.0 {
+            break;
+        }
+    }
+
+    assert_eq!(
+        Some(144328.0),
+        metric.sum(
+            Query::new(TimeRange::new(start_time, end_time))
+                .with_tags_filter(TagsFilter::Or(vec![tags_list[0].to_string(), tags_list[1].to_string()]))
+        )
     );
 }
